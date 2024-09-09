0a472b961da53946bbc8911a79e01790464a4dd0
Prasanna350 <pashadeviprasanna@gmail.com>
2024-09-09 21:43:35 +0530

    Update index.js

--- frontend/src/components/NotFound/index.js
+++ frontend/src/components/NotFound/index.js
@@ -1,9 +1,11 @@
+import "./index.css"
+
 const Notfound = () => {
     return(
-        <div>
+        <div className = "not-found-card">
             <img 
-            src = "https://img.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg?t=st=1724739619~exp=1724743219~hmac=4babeee042a3f3be90c8c3d94d5796e113618d731dab436fb243178406ecd76b&w=996"
-            alt = "not-found"
+            src = "https://www.cloudns.net/blog/wp-content/uploads/2023/10/Error-404-Page-Not-Found.png"
+                alt = "not-found"
             />
         </div>
     )
